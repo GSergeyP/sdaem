cd5844c66c44f64b3a7c4691fc9e31fb0e539270
GSergeyP <psv1981@inbox.ru>
2023-03-25 21:35:53 +0500

    error

--- src/templates/page.Basic/error/error.Const.ts
+++ src/templates/page.Basic/error/error.Const.ts
@@ -20,7 +20,7 @@ const linksData = [
   {
     classes: style.a,
     title: 'Вернуться на главную',
-    url: '',
+    url: 'sdaem/',
     icon: {
       icon: 'house',
       classes: style['a-icon'],
